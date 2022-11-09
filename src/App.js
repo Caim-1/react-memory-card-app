6c7c689f8adb2ab7a6102b152f5fa1d8bf7ad046
Szymon Siudzinski <NaktisPL@gmail.com>
2022-11-09 20:01:49 +0100

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,71 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import cards from './cards';
 
-function App() {
+const App = () => {
+  const [currentScore, setCurrentScore] = useState(0);
+  const [highScore, setHighScore] = useState(0);
+  const [cardArray, setCardArray] = useState([]);
+  const [clickedCards, setClickedCards] = useState([]);
+
+  function shuffle(array) {
+    const shuffledArray = array.sort(() => 0.5 - Math.random());
+    setCardArray([...shuffledArray]);
+  }
+
+  function checkClickedCard(card) {
+    if (clickedCards.includes(card)) {
+      setClickedCards([]);
+      setCurrentScore(0);
+    } else {
+      setClickedCards([...clickedCards, card]);
+      setCurrentScore(currentScore + 1);
+    }
+  }
+
+  useEffect(() => {
+    setCardArray(cards);
+    console.log('Initial load.');
+  }, []) // On initial page load.
+  
+  useEffect(() => {
+    console.log('Array change.');
+  }, [cardArray]) // Every time the array changes.
+
+  useEffect(() => {
+    console.log(clickedCards);
+  }, [clickedCards]) // Every time the array changes.
+
+  useEffect(() => {
+    if (currentScore > 0 && highScore < currentScore) {
+      setHighScore(currentScore);
+    }
+  }, [currentScore, highScore]) // Every time the array changes.
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='Header'>
+        <div className='title'>Memory Game</div>
+        <div className='scores'>
+          <div>Score: {currentScore}</div>
+          <div>High Score: {highScore}</div>
+        </div>
+      </div>
+      <div className='Main'>
+        <div className='card-container'>
+          {cardArray.map((card) =>
+            <div className='card'
+              key={card.name}
+              onClick={() => { 
+                shuffle(cardArray);
+                checkClickedCard(card.name);
+                }}>
+              <img className='card-image' src={card.image} alt={card.name} id='cardImg' />
+              <label>{card.name}</label>
+            </div>
+          )}
+        </div>
+      </div>
     </div>
   );
 }
---
+++ src/cards.js
@@ -0,0 +1,45 @@
+import DemonlordBelzenlok from './images/DemonlordBelzenlok.jpg'
+import EleshNorn from './images/EleshNornGrandCenobite.jpg';
+import InvokeDespair from './images/InvokeDespair.jpg';
+import MurderousRider from './images/MurderousRider.jpg';
+import PhyrexianObliterator from './images/PhyrexianObliterator.jpg';
+import Sheoldred from './images/SheoldredtheApocalypse.jpg';
+import Tergrid from './images/TergridGodofFright.jpg';
+import Thoughtseize from './images/Thoughtseize.jpg';
+
+const cards = [
+  {
+    name: 'Demonlord Belzenlok',
+    image: DemonlordBelzenlok
+  },
+  {
+    name: 'Elesh Norn, Grand Cenobite',
+    image: EleshNorn
+  },
+  {
+    name: 'Invoke Despair',
+    image: InvokeDespair
+  },
+  {
+    name: 'Murderous Rider',
+    image: MurderousRider
+  },
+  {
+    name: 'Phyrexian Obliterator',
+    image: PhyrexianObliterator
+  },
+  {
+    name: 'Sheoldred, the Apocalypse',
+    image: Sheoldred
+  },
+  {
+    name: 'Tergrid, God of Fright',
+    image: Tergrid
+  },
+  {
+    name: 'Thoughtseize',
+    image: Thoughtseize
+  }
+];
+
+export default cards;
